5fb21a77a70747c70b0011c7faf815915b4ceba6
Pavloffff <pavlov.id.2003@gmail.com>
2023-09-28 20:02:56 +0300

    db api docker

--- TaskMgr/TaskMgrAPI/Program.cs
+++ TaskMgr/TaskMgrAPI/Program.cs
@@ -8,7 +8,7 @@ namespace TaskMgrAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            //builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
