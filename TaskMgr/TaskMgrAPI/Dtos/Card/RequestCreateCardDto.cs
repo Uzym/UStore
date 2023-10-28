5c3ff1dd4fbc3f6706fbeab90bc8edd3dad91444
User <lyutoev2003ia@gmail.com>
2023-10-28 18:19:40 +0300

    add section service

--- TaskMgr/TaskMgrAPI/Dtos/Card/RequestCreateCardDto.cs
+++ TaskMgr/TaskMgrAPI/Dtos/Card/RequestCreateCardDto.cs
@@ -2,9 +2,9 @@
 
 public class RequestCreateCardDto
 {
-    public string title { get; set; }
-    public string description { get; set; }
+    public string? title { get; set; }
+    public string? description { get; set; }
     public DateTime? due { get; set; }
-    public List<string> tags { get; set; }
-    public long section_id { get; set; }
+    public List<string>? tags { get; set; }
+    public long? section_id { get; set; }
 }
