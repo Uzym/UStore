d9fb8b1a427876dadd27fb53a8a68a8d3959ad10
User <lyutoev2003ia@gmail.com>
2023-11-05 02:07:09 +0300

    раздробил main.py для красоты

--- TaskMgr/TaskMgrTgBot/main.py
+++ TaskMgr/TaskMgrTgBot/main.py
@@ -14,27 +14,7 @@ from src.handlers import main_setup, project_setup, section_setup, card_setup
 logger = logging.getLogger(__name__)
 
 
-async def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(filename)s:%(lineno)d #%(levelname)-8s "
-        "[%(asctime)s] - %(name)s - %(message)s",
-    )
-
-    logger.info("Starting bot")
-
-    config: Config = load_config()
-
-    storage: MemoryStorage = MemoryStorage()
-
-    bot: Bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
-    dp: Dispatcher = Dispatcher(storage=storage)
-
-    await bot.set_my_name(BOT_NAME)
-    await bot.set_my_description(BOT_DESCRIPTION)
-    await bot.set_my_short_description(BOT_SHORT_DESCRIPTION)
-    await bot.set_my_commands(COMMANDS)
-
+def init_services(dp: Dispatcher, config: Config):
     role_service = RoleService(api_key=config.api_key, logger=logger)
     dp["role_service"] = role_service
     card_service = CardService(api_key=config.api_key, logger=logger)
@@ -46,8 +26,8 @@ async def main():
     project_service = ProjectService(api_key=config.api_key, logger=logger)
     dp["project_service"] = project_service
 
-    dp["logger"] = logger
 
+def setup_routers(dp: Dispatcher):
     main_router, main_dialog = main_setup()
     project_router, project_dialog = project_setup()
     section_router, section_dialog = section_setup()
@@ -64,6 +44,31 @@ async def main():
     dp.include_router(card_dialog)
 
     setup_dialogs(dp)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s "
+        "[%(asctime)s] - %(name)s - %(message)s",
+    )
+
+    logger.info("Starting bot")
+
+    config: Config = load_config()
+
+    storage: MemoryStorage = MemoryStorage()
+
+    bot: Bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    dp: Dispatcher = Dispatcher(storage=storage)
+
+    init_services(dp=dp, config=config)
+    setup_routers(dp=dp)
+
+    await bot.set_my_name(BOT_NAME)
+    await bot.set_my_description(BOT_DESCRIPTION)
+    await bot.set_my_short_description(BOT_SHORT_DESCRIPTION)
+    await bot.set_my_commands(COMMANDS)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
